1992cfb39cba714a94767da216c3c3abbc8ecf8b
Gustavo Matheus Morinaga Cardoso <gudesenhista27@hotmail.com>
2020-07-30 07:45:19 -0300

    Inclusão de Back-End e Formulário de Categorias

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 export const FooterBase = styled.footer`
   background: var(--black);
-  border-top: 2px solid var(--primary);
+  border-top: 2px solid transparent;
+  border-image: linear-gradient(45deg, var(--primary), purple);
+  border-image-slice: 1;
   padding-left: 16px;
   padding-right: 16px;
   padding-top: 32px;
--- src/pages/cadastro/categoria/index.js
+++ src/pages/cadastro/categoria/index.js
@@ -1,14 +1,85 @@
-import React from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
-const CadastroCategoria = () => (
+const CadastroCategoria = () => {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '#ffffff',
+  };
+
+  const [categorias, setCategorias] = useState([]);
+  const [values, setValues] = useState(valoresIniciais);
+
+  const setValue = (chave, valor) => {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  };
+
+  const handleChange = (e) => setValue(e.target.getAttribute('name'), e.target.value);
+
+  const CategoriaCor = styled.code`
+        color: ${values.cor};
+        margin: 0;
+        margin-left: 1rem;
+        display: inline;
+        transition: color .3s ease;
+    `;
+
+  useEffect(() => {
+    const url = 'http://localhost:8080/categorias';
+    fetch(url).then(async (res) => {
+      const resposta = await res.json();
+      setCategorias([
+        ...resposta,
+      ]);
+      console.log(resposta);
+    });
+  }, []);
+
+  return (
     <PageDefault>
-        <h1>Cadastro de Categoria</h1>
-        <Link to="/">
-            Voltar para a Home
-        </Link>
-    </PageDefault>
-)
+      <h1>
+        Cadastro de Categoria:
+        <CategoriaCor>{values.nome}</CategoriaCor>
+      </h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setCategorias([...categorias, values]);
 
-export default CadastroCategoria
+        setValues(valoresIniciais);
+      }}
+      >
+        <FormField tag="input" label="Nome da Categoria" type="text" name="nome" value={values.nome} onChange={handleChange} placeholder="Digite a Categoria" />
+        <FormField tag="textarea" label="Descrição" type="text" name="descricao" value={values.descricao} onChange={handleChange} placeholder="Digite a Descrição" />
+        <FormField tag="input" label="Cor" type="color" name="cor" value={values.cor} onChange={handleChange} />
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+      {categorias.length === 0 && (
+      <div>
+        Carregando...
+      </div>
+      )}
+      <ul>
+        {categorias.map((categoria) => (
+          <li key={`${categoria.nome}`}>
+            {categoria.nome}
+          </li>
+        ))}
+      </ul>
+      <Link to="/">
+        Voltar para a Home
+      </Link>
+    </PageDefault>
+  );
+};
+
+export default CadastroCategoria;
